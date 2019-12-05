40a070cf74cfc19ccaf1460648609efb70243d8f
MarshallCharles <cjm.pcan2@gmail.com>
2019-12-05 16:19:17 +0100

    sequential code

--- project/util.h
+++ project/util.h
@@ -15,10 +15,19 @@ void fillMatrix(int m, int n, int mat[m][n], int maximum) {
     }
   }
 }
-void transposeMatrix(int m, int n, int mat[m][n], int mat2[n][m]){
+void transposeMatrix(int m, int n, int mat1[m][n], int mat2[n][m]){
   for (int i = 0; i < n; i++){
     for (int j = 0; j < m; j++){
-      mat2[i][j] = mat[j][i];
+      mat2[i][j] = mat1[j][i];
+    }
+  }
+}
+void mult_mxn_nxm(int m, int n, int mat1[m][n], int mat2[n][m], int mat3[m][m]){
+  for(int i = 0; i < m; i++){
+    for (int j = 0; j < m; j++){
+      for (int k = 0; k < n; k++){
+        mat3[i][j] += mat1[i][k] * mat2[k][j];
+      }
     }
   }
 }
